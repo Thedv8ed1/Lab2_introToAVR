2c1cadaeccc76b47da38837b15e8fdc37bc4f9a8
Jason Sadler <jsadl003@wch136-24.cs.ucr.edu>
2020-03-31 18:19:28 -0700

    First Compile

--- source/main.c
+++ source/main.c
@@ -1,6 +1,6 @@
 /*	Author: jsadl003
  *  Partner(s) Name: Jason Sadler
- *	Lab Section:
+ *	Lab Section: 021
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
@@ -14,10 +14,11 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRB = 0xFF; // Configure port B's 8 pins as outputs
+    PORTB = 0x00; // Initialize PORTB output to 0’s
     /* Insert your solution below */
     while (1) {
-
+PORTB = 0x0F; // Writes port B's 8 pins with 00001111
     }
     return 1;
 }
