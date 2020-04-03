e23049af73c7ed52a46776be63dc5cfd711bf7f0
Jason Sadler <jsadl003@wch136-24.cs.ucr.edu>
2020-04-02 17:33:22 -0700

    changed simAVRHeader.h and main.c for Reading from input pins section

--- source/main.c
+++ source/main.c
@@ -14,11 +14,12 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
     DDRB = 0xFF; // Configure port B's 8 pins as outputs
     PORTB = 0x00; // Initialize PORTB output to 0’s
     /* Insert your solution below */
     while (1) {
-PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+        PORTB = 0x0F; // Writes port B's 8 pins with 00001111
     }
     return 1;
 }
