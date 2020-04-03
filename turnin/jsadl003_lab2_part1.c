d9a85a60c45c8a7a80431bf1a2d684579183b3e7
Prince Ali Ababwa <thedv8ed1@hotmail.com>
2020-04-02 21:08:10 -0700

    Update jsadl003_lab2_part1.c

--- turnin/jsadl003_lab2_part1.c
+++ turnin/jsadl003_lab2_part1.c
@@ -2,7 +2,11 @@
  *      Partner(s) Name: Jason Sadler
  *	Lab Section: 021
  *	Assignment: Lab #2  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
+ *	Exercise Description: Garage open at night-- A garage door sensor connects to
+ 				PA0 (1 means door open), and a light sensor connects
+				to PA1 (1 means light is sensed). Write a program that
+				illuminates an LED connected to PB0 (1 means illuminate)
+				if the garage door is open at night.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
