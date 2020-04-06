07cb9094b1bfd0d0af9b16263978d598f8bf5cfe
Jason Sadler <jsadl003@wch136-24.cs.ucr.edu>
2020-04-06 13:05:46 -0700

    Fixed part 2

--- turnin/jsadl003_lab2_part2.c
+++ turnin/jsadl003_lab2_part2.c
@@ -1,13 +1,8 @@
 /*	Author: jsadl003
  *      Partner(s) Name: Jason Sadler
  *	Lab Section: 021
- *	Assignment: Lab #2  Exercise #2
- *	Exercise Description: Port A's pins 3 to 0, each connect to a parking space sensor, 
-                            1 meaning a car is parked in the space, of a four-space parking lot. 
-                            Write a program that outputs in binary on port C the number of available 
-                            spaces (Hint: declare a variable "unsigned char cntavail"; you can assign 
-                            a number to a port as follows: PORTC = cntavail;).
-
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -15,11 +10,13 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+//#include <math.h>
+
+
 #endif
 
 int main(void) {
-    DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs
-    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+ DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs
     DDRC = 0xFF; PORTC = 0x00;
     unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
     unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
@@ -28,7 +25,7 @@ int main(void) {
               unsigned char cntavail;
       unsigned short i;
       cntavail = 0;
-      for(i = 0; i < 4; i++){
+      for(i = 0; i < 5; i++){
          if(PINA>>i & 0x01){
             cntavail = cntavail + 1;   
          }
