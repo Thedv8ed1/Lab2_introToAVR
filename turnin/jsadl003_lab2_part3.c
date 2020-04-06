6bd291326423ad49b0b40d1ebfd64af7189b4162
Jason Sadler <jsadl003@wch136-24.cs.ucr.edu>
2020-04-06 13:07:56 -0700

    Fixed part 3

--- turnin/jsadl003_lab2_part3.c
+++ turnin/jsadl003_lab2_part3.c
@@ -1,10 +1,8 @@
 /*	Author: jsadl003
  *      Partner(s) Name: Jason Sadler
  *	Lab Section: 021
- *	Assignment: Lab #2  Exercise #3
- *	Exercise Description: Extend the previous program to still write 
-                            the available spaces number, but only to PC3..PC0, 
-                            and to set PC7 to 1 if the lot is full.
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -12,20 +10,22 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+//#include <math.h>
+
+
 #endif
 
 int main(void) {
-    DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs
-    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+ DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs
     DDRC = 0xFF; PORTC = 0x00;
-   // unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-   // unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+    unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
 
     while(1){
-      unsigned char cntavail;
+              unsigned char cntavail;
       unsigned short i;
       cntavail = 0;
-      for(i = 0; i < 4; i++){
+      for(i = 0; i < 5; i++){
          if(PINA>>i & 0x01){
             cntavail = cntavail + 1;   
          }
