8c9d06f1064a2bc719c7ff42dcccff88b64f9679
Jason Sadler <jsadl003@wch136-24.cs.ucr.edu>
2020-04-06 16:14:28 -0700

    fixed part4

--- turnin/jsadl003_lab2_part4.c
+++ turnin/jsadl003_lab2_part4.c
@@ -1,7 +1,7 @@
 /*	Author: jsadl003
  *      Partner(s) Name: Jason Sadler
  *	Lab Section: 021
- *	Assignment: Lab #2  Exercise #4
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -20,25 +20,38 @@ int main(void) {
     DDRB = 0x00; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
     DDRC = 0x00; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
-    unsigned char tmpA = 0x00; // Temporary variable to hold the value of B
-    unsigned char tmpB = 0x00; // Temporary variable to hold the value of A
-    unsigned char tmpC = 0x00;
-    unsigned char tmpD = 0x00;
+    unsigned int tmpA = 0x00; // Temporary variable to hold the value of B
+    unsigned int tmpB = 0x00; // Temporary variable to hold the value of A
+    unsigned int tmpC = 0x00;
+    unsigned int tmpD = 0x00;
 
     while(1){
       	tmpA = PINA;
 	tmpB = PINB;
 	tmpC = PINC;
         tmpD = 0x00;
-	if((tmpA + tmpB + tmpC) > 0x8C){
+	unsigned  int weight = (tmpA + tmpB + tmpC);
+        if(weight >= 0x8D){
 	    tmpD = 0x01;
+	    
 	}
 	if(fabs(tmpA - tmpC) >0x50){
-	   // tmpD = PORTD;
+	 
 	    tmpD = (tmpD) | (0x02);
 	   
 	}
+	
+	weight = weight >> 2;
+	weight = weight & 0xFC;
+	
+	tmpD = weight + tmpD;
 	PORTD = tmpD;
-    }    
+
+
+
+
+
+}
+        
     return 0;
 }
